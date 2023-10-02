48b8e0ed6abdb10f1a6622588f0629b9e25a2c85
Jairus <jairus.v.tanaka@outlook.com>
2023-10-01 21:56:38 -0700

    feat: universal parameters

--- nodes/ParameterExpression.ts
+++ nodes/ParameterExpression.ts
@@ -5,8 +5,8 @@ import { TypeExpression } from "./TypeExpression.js";
 
 export class ParameterExpression extends Statement {
     public name: Identifier;
-    public type: TypeExpression;
-    constructor(name: Identifier, type: TypeExpression) {
+    public type: TypeExpression | null = null;
+    constructor(name: Identifier, type: TypeExpression | null = null) {
         super();
         this.name = name;
         this.type = type;
