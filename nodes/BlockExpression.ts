72eb516a2d990c99ef809cd2e7e537707507a901
Jairus <jairus.v.tanaka@outlook.com>
2023-10-01 21:55:46 -0700

    localized blocks

---
+++ nodes/BlockExpression.ts
@@ -0,0 +1,12 @@
+import { Token, TokenData } from "../src/tokenizer.js";
+import { Expression } from "./Expression.js";
+import { Statement } from "./Statement.js";
+
+export class BlockExpression extends Expression {
+    constructor(public statements: Statement[]) {
+        super();
+    }
+    static match: ((tok: TokenData) => boolean)[] = [
+        (tok) => tok.token === Token.LeftBracket
+    ]
+}
