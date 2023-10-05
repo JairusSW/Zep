cbf8fd3ed41c81bd484caa894334280ded711111
Jairus <jairus.v.tanaka@outlook.com>
2023-10-04 21:14:43 -0700

    feat: output wasm for add

---
+++ test.ts
@@ -0,0 +1,6 @@
+const binary = Bun.file("./build/add.wasm");
+
+const module = new WebAssembly.Module(await binary.arrayBuffer());
+const instance = new WebAssembly.Instance(module, {});
+
+console.log(instance.exports.add(3,9))
