d5fbe74e5a09e4bb91a38bb25b956b6b21bc7dc1
Jairus Tanaka <me@jairus.dev>
2025-12-09 21:45:30 -0500

    add: support for property accesses

---
+++ src/ast/nodes/PropertyAccessExpression.ts
@@ -0,0 +1,16 @@
+import { Range } from "../Range.js";
+import { Expression } from "./Expression.js";
+import { Identifier } from "./Identifier.js";
+import { Statement } from "./Statement.js";
+
+export class PropertyAccessExpression extends Statement {
+  public nameOf: string = "PropertyAccessExpression";
+  public expression: Expression;
+  public property: Identifier;
+  constructor(expression: Expression, property: Identifier, range: Range) {
+    super();
+    this.expression = expression;
+    this.property = property;
+    this.range = range;
+  }
+}
